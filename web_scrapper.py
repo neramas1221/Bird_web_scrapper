bfc1c97b044e874142545925f735fec2f4466ba3
neramas1221 <connorwheeler1997@gmail.com>
2018-10-18 13:25:40 +0100

    Fixed problem of being unable to find correct table

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,4 +1,17 @@
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 import numpy as np
+import webbrowser
 
+page_counter = 1
+page_url = "https://www.xeno-canto.org/explore?dir=0&order=xc&pg="
+print(page_url + str(page_counter))
+page = requests.get(page_url+ str(page_counter))
+
+soup = BeautifulSoup(page.text,'lxml')
+
+table = soup.find(class_="results")#'table',attrs={'class':'results'})
+rows = table.find_all('tr')
+cols = []
+for row in rows:
+    cols.append(row.find_all('td'))
