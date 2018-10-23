3a988ca886a2ea32b3cf05541f09ded8e2e17ef6
neramas1221 <connorwheeler1997@gmail.com>
2018-10-23 15:58:11 +0100

    added panders data frames and while loop

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-
+import numpy as np
+import pandas as pd
 
 page_counter = 1
 page_url     = "https://www.xeno-canto.org/explore?dir=0&order=xc&pg="
@@ -11,20 +12,22 @@ folder       = "./sounds/"
 soup         = BeautifulSoup(page.text, 'lxml')
 table        = soup.find(class_="results")
 rows         = table.find_all('tr') """
-data         = []
-data_two     = []
+row_Data     = []
+col_Data     = []
 cont         = True
+table_data   = np.array([])
 
 while cont:
     print("loop " + str(page_counter))
-    data         = []
+    row_Data     = []
     page         = requests.get(page_url + str(page_counter))
+    print(page_url + str(page_counter))
     soup         = BeautifulSoup(page.text, 'lxml')
     table        = soup.find(class_="results")
     rows         = table.find_all('tr')
     for row in range(1, len(rows)):
         cols     = rows[row].find_all('td')
-        data_two = []
+        col_Data = []
         for col in range(1, len(cols)):
 
             info = cols[col].contents[0]
@@ -46,32 +49,46 @@ while cont:
                     section = cols[col].find(class_='selected')
                     rating  = section.contents[0]
                     rating  = rating.contents[0]
-                    data_two.append(rating)
+                    col_Data.append(rating)
                 else:
-                    data_two.append(" ")
+                    col_Data.append(" ")
 
             info = " ".join(str(info).split())
-            data_two.append(info)
-        data.append(data_two)
+            col_Data.append(info)
+        row_Data.append(col_Data)
     f = open("test.txt", "w")
-    for i in range(0, len(data)):
-        for j in range(0, len(cols)-1):
-            f.write(str(data[i][j]) + "\n\n")
+    for i in range(0, len(row_Data)):
+        for j in range(0, len(cols)):
+            f.write(str(row_Data[i][j]) + "\n\n")
 
     f.close()
-    for i in range(0, len(data)):
+    for i in range(0, len(row_Data)):
 
-        print("Downloading...")
-        r = requests.get(download_url + str(data[i][11]), stream=True)
-        with open(folder+str(data[i][12])+".mp3", 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-        f.close()
-        print("...Done")
-        time.sleep(0.1)
+       print("Downloading...")
+       r = requests.get(download_url + str(row_Data[i][11]), stream=True)
+       with open(folder+str(row_Data[i][12])+".mp3", 'wb') as f:
+           for chunk in r.iter_content(chunk_size=1024):
+               if chunk:
+                   f.write(chunk)
+       f.close()
+       print("...Done")
 
-    if len(cols) != 30:
+    time.sleep(0.1)
+    
+    if page_counter == 1:
+        table_data = np.array(row_Data)
+    else:
+        table_data = np.vstack((table_data, row_Data))
+    
+    if page_counter >=3:#len(row_Data) != 30:
         cont = False
     else:
         page_counter = page_counter + 1
+
+output = pd.DataFrame(table_data,columns=['Common name','Length','Recordist','Date','Time','Country',
+                                          'Location','Elev. (m)','Type','Remarks','Rating','Download link',
+                                          'ID'])
+output.to_csv("data_set.csv")
+print(table_data.size)
+print(table_data.shape)
+#print(table_data)
